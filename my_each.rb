72f67e3ffade0b838ec4aa2a3bdfa3324f1a59c7
Robin Tram <robin.tram@gmail.com>
2018-09-20 17:51:33 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,12 @@
 def my_each(array)
   if block_given?
     counter = 0 
+    
     while counter < array.length
       yield array[counter]
       counter += 1
     end 
+    
     array
   else 
     puts "Hey! No block was given!"
