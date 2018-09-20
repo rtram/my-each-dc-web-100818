e6475f04c91427f7eba0a0205ef4926747f17b1e
Robin Tram <robin.tram@gmail.com>
2018-09-20 17:23:48 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  new_array = []
+  counter = 0 
+  yield 
 end
+
+def my_each do 
+  while counter < collection.length
+  puts collection[counter]
+  end 
+end 
