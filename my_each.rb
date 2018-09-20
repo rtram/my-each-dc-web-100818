3b3f4189a97fc1aca20ff3cff25a61ec45cdb380
Robin Tram <robin.tram@gmail.com>
2018-09-20 17:24:39 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,12 @@
 def my_each(array)
   new_array = []
   counter = 0 
-  yield 
+  yield array
 end
 
 def my_each do 
   while counter < collection.length
   puts collection[counter]
+  counter += 1
   end 
 end 
