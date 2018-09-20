d3b5422c371187d4bce92e4291c60fa0f0c0991e
Robin Tram <robin.tram@gmail.com>
2018-09-20 17:52:44 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    counter = 0 
+    
+    while counter < array.length
+      yield array[counter]
+      counter += 1
+    end 
+    
+    array
+  else 
+    "Hey! No block was given!"
+  end
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  i
 end
